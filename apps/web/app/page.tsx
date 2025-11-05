795b78145c2aa99e399beb7b3bdee2a498c5458b
FabricioAcuna <fabricio.acuna@chasacademy.se>
2025-11-05 14:05:38 +0100

    tailwind off

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,10 @@
 import Navbar from "./components/navbar";
 
-
-
 export default function Page() {
   return (
     <>
       <main className="flex justify-center">
-        <Navbar/>
+        <Navbar />
         <h1>Välkommen till vår butik!</h1>
         <h2 className=" text-red-500">hej</h2>
       </main>
