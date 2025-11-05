3094c2da2396ba4ff7a6fcf5d3eadb4e805a6e86
FabricioAcuna <fabricio.acuna@chasacademy.se>
2025-11-05 13:14:21 +0100

    test

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,13 @@
 import Navbar from "./components/navbar";
 
+
+
 export default function Page() {
   return (
     <>
       <main className="flex justify-center">
         <h1>Välkommen till vår butik!</h1>
+        <h2 className=" text-red-500">hej</h2>
       </main>
     </>
   );
