7a67b2c4dff54bf262d5931328b08ebaa1577990
akansson <ahmetakan95@gmail.com>
2025-11-05 15:00:55 +0100

    Reset content to commit 8d0ddd1 (preserve full history)

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,11 @@
 import Navbar from "./components/navbar";
 
+
+
 export default function Page() {
   return (
     <>
       <main className="flex justify-center">
-        <Navbar />
         <h1>Välkommen till vår butik!</h1>
         <h2 className=" text-red-500">hej</h2>
       </main>
